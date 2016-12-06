25b533b211c82e18bde60fb9ed8373d009b206f1
Jay Patel <jcp313@gmail.com>
2016-12-06 13:58:03 -0500

    added archiveLink method

--- FinalProjectAlpha/FinalProjectAlpha/Controllers/ProjectController.cs
+++ FinalProjectAlpha/FinalProjectAlpha/Controllers/ProjectController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,8 +30,35 @@ namespace FinalProjectAlpha.Controllers
 
             dbContext.SaveChanges();
 
+
+
            return RedirectToAction("Details", Link);
            
         }
+
+        public string archiveLink(string inputUrl)
+        {
+            //Use Wayback Machine Api + users project link
+            HttpWebRequest request = WebRequest.CreateHttp("http://archive.org/wayback/available?url=" + inputUrl);
+            request.UserAgent =
+    @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+
+            //Cast Wayback request to Response.  
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Read response Url data and convert into string.
+            StreamReader read = new StreamReader(response.GetResponseStream());
+            string waybackResponse = read.ReadToEnd();
+
+            //Parse from string to object.
+            JObject urlResponse = JObject.Parse(waybackResponse);
+
+            //cast exact part of return url needed and save as archiveUrl string
+            string archiveUrl = (string)urlResponse["archived_snapshots"]["url"];
+
+            return archiveUrl;
+
+        }
+
     }
 }

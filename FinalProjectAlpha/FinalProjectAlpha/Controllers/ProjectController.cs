02d7852f6d75c95018053a3f90c6c0f09fb0b95a
Thomas Justin Krueger <Tomjk@acdmail.com>
2016-12-06 17:01:52 -0500

    minor changes to save action

--- FinalProjectAlpha/FinalProjectAlpha/Controllers/ProjectController.cs
+++ FinalProjectAlpha/FinalProjectAlpha/Controllers/ProjectController.cs
@@ -25,14 +25,21 @@ namespace FinalProjectAlpha.Controllers
         {
             //get db
             waybackdbEntities dbContext = new waybackdbEntities();
+
+            saveLink(Link);
+
             //get Link from Jay's ArchiveLink()
             string ArchiveLink = archiveLink(Link);
             //add Archive obj to db
             Archive archive = new Archive(Link, ArchiveLink, RepoLink, ShortDesc, LongDesc);
+
+
             dbContext.Archives.Add(archive);
             //save to db
             dbContext.SaveChanges();
             //send user to Project/Details 
+
+
             return RedirectToAction("Details", Link);
 
         }

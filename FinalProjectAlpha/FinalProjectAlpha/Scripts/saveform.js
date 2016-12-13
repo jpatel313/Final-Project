eceede8acf2eb035aaa0fc95949da9ffb38aede7
J Patel <jcp313@gmail.com>
2016-12-13 14:52:52 -0500

    error message url fixes

--- FinalProjectAlpha/FinalProjectAlpha/Scripts/saveform.js
+++ FinalProjectAlpha/FinalProjectAlpha/Scripts/saveform.js
@@ -1,43 +1,53 @@
-﻿$(function () {
-    $("#save-proj").validate(
-        {
-            rules: {            //set rules for validation
+﻿/// <reference path="jquery.validate.js" />
+/// <reference path="jquery-3.1.1.intellisense.js" />
+/// <reference path="respond.js" />
+
+
+JQuery (function ($) {
+    var validator =  $('#save-proj').validate ({
+      
+        rules: { //set rules for validation
+            Link: {
+                required: true,
+                url: true,
+                regexp: /^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i
+            },
+            RepoLink: {
+                required: false,
+                url: true,
+                regexp: /^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i
+            },
+            ShortDesc: {
+                required: true,
+                rangelength: [10, 200]
+            },
+            LongDesc: {
+                required: false,
+                rangelength: [100, 500]
+            }
+        },
+        messages:
+            { //error messages
                 Link: {
-                    required: true,
-                    url: true,
-                    regexp: /^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i
+                    required: "Your Project URL is required.",
+                    url: "Please enter a valid, working URL."
                 },
                 RepoLink: {
-                    required: false,
-                    url: true,
-                    regexp: /^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i
+                    url: "Please enter a valid, working URL."
                 },
                 ShortDesc: {
-                    required: true,
-                    rangelength: [10, 200]
+                    required: "Short project description is required.",
+                    rangelength: "Your short description should be between 10 and 200 characters."
                 },
                 LongDesc: {
-                    required: false,
-                    rangelength: [100, 500]
+                    rangelength: "Your long description should be between 100 and 500 characters."
                 }
             },
-            messages:
-                { //error messages
-                    Link: {
-                        required: "Your Project URL is required.",
-                        url: "Please enter a valid, working URL."
-                    },
-                    RepoLink: {
-                        url: "Please enter a valid, working URL."
-                    },
-                    ShortDesc: {
-                        required: "Short project description is required.",
-                        rangelength: "Your short description should be between 10 and 200 characters."
-                    },
-                    LongDesc: {
-                        rangelength: "Your long description should be between 100 and 500 characters."
-                    }
-                }
-        });
-
-});
+        highlight: function (element) {
+            $(element).parent().addClass('error')
+        },
+        unhighlight: function (element) {
+            $(element).parent().removeClass('error')
+        }
+    });
+})

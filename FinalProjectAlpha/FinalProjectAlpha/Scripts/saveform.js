203d8876fe520f22c2305fbf09b3fa4939285910
Mary Olive <mary.olive@me.com>
2016-12-16 08:57:30 -0500

    Remove Reg Expressions from saveform.js to fix bug in Save form

--- FinalProjectAlpha/FinalProjectAlpha/Scripts/saveform.js
+++ FinalProjectAlpha/FinalProjectAlpha/Scripts/saveform.js
@@ -12,15 +12,11 @@ $(function () {
             Link: {
                 required: true,         //causes the form field named "Link" to be required
                 url: true,         //requires valid URL input in the form field named "Link" (JQuery checks and flags on submit if it is not)
-                //regexp may be redundant.  is a pattern the input should conform to (permits http, https, etc.)
-                regexp: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i
-            },
+                   },
             RepoLink: {
                 required: false,        //causes the form field named "RepoLink" to not be required
                 url: true,//requires URL input in the form field named "RepoLink" (JQuery checks and flags on submit if it is not)
-                //regexp may be redundant.  is a pattern the input should conform to (permits ftp and https, etc.)
-                regexp: /^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i
-            },
+                  },
             ShortDesc: {
                 required: true,              //causes the form field named "ShortDesc" to not be required
                 rangelength: [10, 200]      //rule controlling upper and lower limits of character number in "ShortDesc"
@@ -34,10 +30,10 @@ $(function () {
             {                    //error messages (to be displayed if the above rules are not met.  displayed when focus of field is lost (after some input only)
                 Link: {
                     required: "Your Project URL is required.",      //message displayed if "Link" input is left blank
-                    url: "Please enter a valid, working URL."
+                    //url: "Please enter a valid, working URL."
                 },
                 RepoLink: {
-                    url: "Please enter a valid, working URL."
+                    //url: "Please enter a valid, working URL."
                 },
                 //If user enters nothing in the required field, and included is the ranges for short and ong description
                 ShortDesc: {

87f3de4f4ac1ad5bcb70fb080ff6b846934f1079
Jay Patel <jcp313@gmail.com>
2016-12-18 12:43:10 -0500

    model updated for validation

--- FinalProjectAlpha/FinalProjectAlpha/Models/AspNetUser.cs
+++ FinalProjectAlpha/FinalProjectAlpha/Models/AspNetUser.cs
@@ -11,7 +11,7 @@ namespace FinalProjectAlpha.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
+    
     public partial class AspNetUser
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
